95ddb4f25d5c2b6dd6d52ca8b5cc0c9f4ddb1383
Danny Ayers <danny.ayers@gmail.com>
2011-08-16 01:05:07 +0200

    tweak

--- crawler/src/hello.js
+++ crawler/src/hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello (buggering) World\n');
-}).listen(80, "67.207.131.83");
+}).listen(80, "data.hyperdata.org"); // 67.207.131.83 data.hyperdata.org
 console.log('Server running at http://127.0.0.1:8888/');
