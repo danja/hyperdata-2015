e88e1ad8fdbf4da58f415553c9e61dfa663f6447
Danny Ayers <danny.ayers@gmail.com>
2011-08-16 00:37:23 +0200

    ip change

--- crawler/src/hello.js
+++ crawler/src/hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello (buggering) World\n');
-}).listen(8888);
+}).listen(80, "67.207.131.83");
 console.log('Server running at http://127.0.0.1:8888/');
