febfff64c61fb1193d974c7f9e7fe0968fbf8e6b
Danny Ayers <danny.ayers@gmail.com>
2012-03-20 14:23:48 +0100

    added reviewer comments

---
+++ docs/FIXME/SPARQL Demo - N2 wiki_files/index.php
@@ -0,0 +1,22 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 22:42:25 Apr 1, 2008 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 21:13:32 Feb 29, 2012.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/* generated javascript */
+var skin = 'monobook';
+var stylepath = '/mediawiki/skins';
+
+/* MediaWiki:Common.js */
+/* Any JavaScript here will be loaded for all users on every page load. */
+
+/* MediaWiki:Monobook.js (deprecated; migrate to Common.js!) */
+/* Deprecated; use [[MediaWiki:common.js]] */
